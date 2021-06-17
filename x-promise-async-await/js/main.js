eb3593de882402a78b7ba3233df33c8ecf61d61a
Joe Cserko <j.cserko@gmail.com>
2021-06-17 17:13:21 +0200

    l01 end

--- x-promise-async-await/js/main.js
+++ x-promise-async-await/js/main.js
@@ -1,1 +1,15 @@
-// main
+fetch('img/ast-async2-pipe.jpg')
+    .then( response => {
+        if (!response.ok) {
+            throw new Error(`Http error! STATUS: ${response.status}`);
+        }
+
+        return response.blob();
+    }).then( myBlob => {
+        let objectURL = URL.createObjectURL(myBlob);
+        let image = document.querySelector('.card.shadow-sm img');
+        image.src = objectURL;
+    })
+    .catch( e => {
+        console.log(`Error: ${e.message}`);
+    });
